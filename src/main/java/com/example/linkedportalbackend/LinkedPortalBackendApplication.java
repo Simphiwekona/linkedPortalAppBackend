45fd3a13b5c6a94144642e808e455c47dc47bee0
simphiwek <simphiwe.olwethu@jumpco.io>
2024-09-15 17:24:15 +0200

    test files

---
+++ src/main/java/com/example/linkedportalbackend/LinkedPortalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.linkedportalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedPortalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedPortalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/linkedportalbackend/LinkedPortalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.linkedportalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedPortalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
